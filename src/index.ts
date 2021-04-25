29c485c043417507d8ccf07caa4bc5f14e1dfa3f
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-04-25 11:51:43 +0900

    公費アカウントへの収入レコードの追加をできるように

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import * as dotenv from 'dotenv'
-import { CurrentZaimClient } from './lib/currentZaimClient'
-dotenv.config()
+import * as Zaim from './zaim'
 
-const currentZaimClient = new CurrentZaimClient()
-currentZaimClient.getAllIncomes().then(result => {
-  console.log(result.length)
-  console.log(result.reduce((max, current) => (max += current.amount), 0))
-})
-currentZaimClient.getAllPublicPayments().then(result => {
-  console.log(result.length)
-  console.log(result.reduce((max, current) => (max += current.amount), 0))
-})
-currentZaimClient.getAllPrivatePayments().then(result => {
-  console.log(result.length)
-  console.log(result.reduce((max, current) => (max += current.amount), 0))
-})
+dotenv.config()
+const currentZaimClient = new Zaim.CurrentZaimClient()
+const publicZaimClient = new Zaim.PublicZaimClient()
+
+/**
+ * 公費用アカウントに全収入レコードを追加する
+ */
+async function addAllIncomesToPublicAccount() {
+  const currentIncomes = await currentZaimClient.getAllIncomes()
+  for (const income of currentIncomes) {
+    await publicZaimClient.duplicateIncomeFrom(income)
+    console.log(`...公費アカウントへの収入レコードを追加 ${income.date}`)
+  }
+}
+
+;(async () => {
+  await addAllIncomesToPublicAccount()
+})()
