c274635375fa645a0c56fa699f943473b11dc499
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-04-25 15:21:09 +0900

    メインロジックを整理

--- src/index.ts
+++ src/index.ts
@@ -8,53 +8,66 @@ const currentZaimClient = new Zaim.CurrentZaimClient()
 const publicZaimClient = new Zaim.PublicZaimClient()
 const privateZaimClient = new Zaim.PrivateZaimClient()
 
-/**
- * 私費用アカウントに全収入レコードを追加する
- */
-async function addAllIncomesToPrivateAccount() {
-  const allDays = Util.monthlyFirstDays()
-  for (const date of allDays) {
-    await privateZaimClient.addPocketMoneyIncome(date, Util.getPocketMoneyBudget(date))
-    console.log(`...私費アカウントへの収入レコードを追加 ${date.format()}`)
+const actions = {
+  toPrivate: {
+    /**
+     * 私費用アカウントにお小遣い収入レコードを新規登録する
+     */
+    async addPocketMoneyIncomes() {
+      const allDays = Util.monthlyFirstDays()
+      for (const date of allDays) {
+        await Util.sleep()
+        await privateZaimClient.addPocketMoneyIncome(date, Util.getPocketMoneyBudget(date))
+        console.log(`...私費アカウントへの収入レコードを追加 ${date.format()}`)
+      }
+    },
+    /**
+     * 私費用アカウントに、私費の支出を転写する
+     */
+    async transcribePayments() {
+      const currentIncomes = await currentZaimClient.getAllIncomes()
+      for (const income of currentIncomes) {
+        await Util.sleep()
+        await publicZaimClient.duplicateIncomeFrom(income)
+        console.log(`...公費アカウントへの収入レコードを追加 ${income.date}`)
+      }
+    }
+  },
+  toPublic: {
+    /**
+     * 公費用アカウントに、お小遣いの支出レコードを新規登録する
+     */
+    async addPocketMoneyPayments() {
+      const allDays = Util.monthlyFirstDays()
+      for (const date of allDays) {
+        await Util.sleep()
+        await publicZaimClient.addPocketMoneyPayment(date, Util.getPocketMoneyBudget(date))
+        console.log(`...公費アカウントへの小遣い支出レコードを追加 ${date.format()}`)
+      }
+    },
+    /**
+     * 公費用アカウントに、給与収入を転写する
+     */
+    async transcribeIncomes() {
+      const currentIncomes = await currentZaimClient.getAllIncomes()
+      for (const income of currentIncomes) {
+        await Util.sleep()
+        await publicZaimClient.duplicateIncomeFrom(income)
+        console.log(`...公費アカウントへの収入レコードを追加 ${income.date}`)
+      }
+    },
+    /**
+     * 公費用アカウントに、公費支出を転写する
+     */
+    async transcribePayments() {
+      const currentPayments = await currentZaimClient.getAllPublicPayments()
+      for (const payment of currentPayments) {
+        await Util.sleep()
+        await publicZaimClient.duplicatePaymentFrom(Util.convertMoneyOption(payment))
+        console.log(`...公費アカウントへの支出レコードを追加 ${payment.date}`)
+      }
+    }
   }
 }
 
-/**
- * 私費用アカウントに全支出レコードを追加する
- */
-async function addAllPaymentsToPrivateAccount() {
-  const currentPayments = await currentZaimClient.getAllPrivatePayments()
-  for (const payment of currentPayments) {
-    await privateZaimClient.createPayment(Util.convertMoneyOption(payment))
-    console.log(`...私費アカウントへの支出レコードを追加 ${payment.date}`)
-  }
-}
-
-/**
- * 公費用アカウントに全収入レコードを追加する
- */
-async function addAllIncomesToPublicAccount() {
-  const currentIncomes = await currentZaimClient.getAllIncomes()
-  for (const income of currentIncomes) {
-    await publicZaimClient.duplicateIncomeFrom(income)
-    console.log(`...公費アカウントへの収入レコードを追加 ${income.date}`)
-  }
-}
-
-/**
- * 公費用アカウントに全支出レコードを追加する
- */
-async function addAllPaymentsToPublicAccount() {
-  const currentPayments = await currentZaimClient.getAllPublicPayments()
-  for (const payment of currentPayments) {
-    await publicZaimClient.createPayment(Util.convertMoneyOption(payment))
-    console.log(`...公費アカウントへの支出レコードを追加 ${payment.date}`)
-  }
-}
-
-;(async () => {
-  await addAllPaymentsToPublicAccount()
-  // await addAllPaymentsToPrivateAccount()
-  // await addAllIncomesToPrivateAccount()
-  // await addAllIncomesToPublicAccount()
-})()
+;(async () => {})()
